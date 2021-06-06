a50701a506a92c5280ab1fe4bc467f8c354161b9
Furkan Arabacı <furkan.arabaci@smartface.io>
2021-06-06 21:52:37 +0300

    change sf-core to native

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-import FlexLayout = require('sf-core/ui/flexLayout');
-import TextBox = require('sf-core/ui/textbox');
-import Button = require('sf-core/ui/button');
+import FlexLayout = require('@smartface/native/ui/flexLayout');
+import TextBox = require('@smartface/native/ui/textbox');
+import Button = require('@smartface/native/ui/button');
 
 export default class KeyboardLayout extends FlexLayout {
   static init: (textBoxes: Array<TextBox> | TextBox) => Array<KeyboardLayout>;
